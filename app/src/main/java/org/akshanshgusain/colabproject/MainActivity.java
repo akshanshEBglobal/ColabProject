570862a2af2a7a3aac2bd67e15d861a220437b78
CodeZERONine <akshanshssj5@gmail.com>
2018-09-25 17:06:10 +0530

    Merge branch 'Himanshu' into Developers
    # Conflicts:
    #	app/src/main/java/org/akshanshgusain/colabproject/MainActivity.java

--- app/src/main/java/org/akshanshgusain/colabproject/MainActivity.java
+++ app/src/main/java/org/akshanshgusain/colabproject/MainActivity.java
@@ -2,6 +2,7 @@ package org.akshanshgusain.colabproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
