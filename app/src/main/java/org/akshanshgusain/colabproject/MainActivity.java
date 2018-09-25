378324d42c26253146d7609ac400aa38a3dbae51
CodeZERONine <akshanshssj5@gmail.com>
2018-09-25 16:53:34 +0530

    Branch Akshansh comment 2

--- app/src/main/java/org/akshanshgusain/colabproject/MainActivity.java
+++ app/src/main/java/org/akshanshgusain/colabproject/MainActivity.java
@@ -9,6 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //This is the Akshansh branch Message 1
+    }
+
+    public void akshansh()
+    {
+        //Akshansh Comment 2
     }
 }
