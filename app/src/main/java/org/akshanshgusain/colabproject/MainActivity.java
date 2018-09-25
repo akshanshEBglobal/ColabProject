c58fd962b169ff6a7e07cdbf9546bf8348fa6243
himanshu.shukla <himanshuraj.shukla@gmail.com>
2018-09-25 16:46:00 +0530

    h2a 1

--- app/src/main/java/org/akshanshgusain/colabproject/MainActivity.java
+++ app/src/main/java/org/akshanshgusain/colabproject/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Akshansh: Hi HImanshu, How you doing bro?
+        //himanshu: Hi akshansh, How you doing bro?
     }
 }
