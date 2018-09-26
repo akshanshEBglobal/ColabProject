15cb092ab567ab7c7450e3bd3b39793fdb62f84a
himanshu.shukla <himanshuraj.shukla@gmail.com>
2018-09-26 10:20:48 +0530

    added commit

--- app/src/main/java/org/akshanshgusain/colabproject/MyNewClass.java
+++ app/src/main/java/org/akshanshgusain/colabproject/MyNewClass.java
@@ -1,4 +1,9 @@
 package org.akshanshgusain.colabproject;
 
 public class MyNewClass {
+
+    public  void  himanshu()
+    {
+        //Toast.makeText(g, "helloo", Toast.LENGTH_SHORT).show();
+    }
 }
