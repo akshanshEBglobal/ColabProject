a04049911862ae6746aa34dc3069a31ca5129b2b
CodeZERONine <akshanshssj5@gmail.com>
2018-09-26 09:13:42 +0530

    DEv comment5

--- app/src/main/java/org/akshanshgusain/colabproject/MainActivity.java
+++ app/src/main/java/org/akshanshgusain/colabproject/MainActivity.java
@@ -2,6 +2,7 @@ package org.akshanshgusain.colabproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,12 +10,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //hii sir how are you..
     }
 
     public void akshansh()
     {
         //Akshansh Comment 2
+    }
+
+    public void akshansh2()
+    {
+        //Akshansh Comment 2
+        Toast.makeText(this, "Random Toast", Toast.LENGTH_SHORT).show();
+        //Why this  ^^^^ toast man?
+
     }
 
     public  void  mytest()
